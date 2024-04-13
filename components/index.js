5be332a2f25c89b4f01f5a09e35e0b821224f014
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-04-13 19:32:04 +0300

    Updated imports

--- components/index.js
+++ components/index.js
@@ -1,27 +1,16 @@
 import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
 
 // home screen
-import Welcome from "./home/welcome/Welcome";
-import Nearbysites from "./home/nearby/Nearbysites";
+import Site from "./home/site/Site";
 
 // job details screen
-import Company from "./jobdetails/company/Company";
-import { default as JobTabs } from "./jobdetails/tabs/Tabs";
-import { default as JobAbout } from "./jobdetails/about/About";
-import { default as JobFooter } from "./jobdetails/footer/Footer";
-import Specifics from "./jobdetails/specifics/Specifics";
+// import Company from "./jobdetails/company/Company";
+// import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+// import { default as JobAbout } from "./jobdetails/about/About";
+// import { default as JobFooter } from "./jobdetails/footer/Footer";
+// import Specifics from "./jobdetails/specifics/Specifics";
 
 // common
-import NearbyJobCard from "./common/cards/nearby/NearbySiteCard";
+// import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
 
-export {
-  ScreenHeaderBtn,
-  Welcome,
-  Nearbysites,
-  Company,
-  JobTabs,
-  JobAbout,
-  JobFooter,
-  Specifics,
-  NearbyJobCard,
-};
+export { ScreenHeaderBtn, Site };
