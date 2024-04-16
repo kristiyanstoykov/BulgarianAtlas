36b719b21320c56d711b23803a32b418d03770ac
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-04-16 15:58:15 +0300

    Moved function for stripping basic html to utils/

--- components/common/cards/site/SiteCard.jsx
+++ components/common/cards/site/SiteCard.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Text, View, Image, TouchableOpacity } from "react-native";
 import styles from "./sitecard.style";
-
-function stripHtmlTags(str) {
-  if (str === null || str === "") return false;
-  else str = str.toString();
-  return str.replace(/<[^>]*>/g, ""); // Regular expression to remove HTML tags
-}
+import stripHtmlTags from "../../../../utils";
 
 export default function SiteCard({ image, title, content, handleNavigate }) {
   const correctedImgUrl = image.replace("localhost", "10.0.2.2");
