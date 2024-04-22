72074459a3407c6f65eaf87dfb7e676b17d602c0
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-04-22 11:30:44 +0300

    Added strip html function

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,3 @@
-export default function stripHtmlTags(str) {
-  if (str === null || str === "") return false;
-  else str = str.toString();
-  return str.replace(/<[^>]*>/g, ""); // Regular expression to remove HTML tags
-}
+import stripHtmlTags from "./stripHtmlTags";
+
+export default stripHtmlTags;
