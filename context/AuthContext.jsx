2190a8c772bba42d77479d0a8b0f8d1fa36b8692
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-23 18:26:12 +0300

    Added register

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,6 +1,8 @@
-import { createContext, useContext, useState } from "react";
 import axios from "axios";
+import { encode as btoa } from "base-64";
+import { createContext, useContext, useState } from "react";
 import { Alert } from "react-native";
+import { WORDPRESS_API_KEY, WORDPRESS_API_USERNAME } from "../env";
 import { stripHtmlTags } from "../utils";
 
 export const ROLES = {
@@ -28,25 +30,16 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (username, password) => {
     try {
-      const response = await axios.post(
-        "http://10.0.2.2/bulgarian-atlas/wp-json/jwt-auth/v1/token",
-        { username, password }
-      );
+      const response = await axios.post("https://bulgarian-atlas.nst.bg/wp-json/jwt-auth/v1/token", {
+        username,
+        password,
+      });
       const data = response.data;
       if (response.status === 200) {
         const { token, profile } = response.data;
-        const {
-          user_nicename,
-          roles,
-          user_first_name,
-          user_last_name,
-          user_email,
-          id,
-        } = profile;
+        const { user_nicename, roles, user_first_name, user_last_name, user_email, id } = profile;
 
-        const userRole =
-          roles.find((role) => Object.values(ROLES).includes(role)) ||
-          ROLES.USER;
+        const userRole = roles.find((role) => Object.values(ROLES).includes(role)) || ROLES.USER;
 
         setAuthState({
           authenticated: true,
@@ -59,17 +52,13 @@ export const AuthProvider = ({ children }) => {
           token: token,
         });
       } else {
-        throw new Error(
-          "Failed to log in with status code: " + response.status
-        );
+        throw new Error("Failed to log in with status code: " + response.status);
       }
     } catch (error) {
       console.error("Login error:", error);
       Alert.alert(
         "Login Failed",
-        error.response
-          ? stripHtmlTags(error.response.data.message)
-          : "Login failed due to network error"
+        error.response ? stripHtmlTags(error.response.data.message) : "Login failed due to network error"
       );
 
       setAuthState({
@@ -84,6 +73,42 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const register = async (name, lastname, email, password) => {
+    try {
+      const username = email.split("@")[0];
+      const token = btoa(`${WORDPRESS_API_USERNAME}:${WORDPRESS_API_KEY}`);
+
+      const response = await axios.post(
+        "https://bulgarian-atlas.nst.bg/wp-json/wp/v2/users",
+        {
+          username: username,
+          first_name: name,
+          last_name: lastname,
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            Authorization: `Basic ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 201 || response.status === 200) {
+        Alert.alert("Registration Successful", "You have registered successfully! Now you need to Login.");
+      } else {
+        throw new Error("Registration failed with status code: " + response.status);
+      }
+    } catch (error) {
+      console.error("Registration error:", error);
+      Alert.alert(
+        "Registration Failed",
+        error.response ? error.response.data.message : "An error occurred during registration"
+      );
+    }
+  };
+
   const logout = () => {
     setAuthState({
       authenticated: false,
@@ -94,6 +119,7 @@ export const AuthProvider = ({ children }) => {
 
   const value = {
     onLogin: login,
+    onRegister: register,
     onLogout: logout,
     authState,
   };
