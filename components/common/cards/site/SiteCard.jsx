bca627b44e97e1eaaee36fc3d16a2819c72459d0
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-25 00:30:28 +0300

    Fixed some styling issues

--- components/common/cards/site/SiteCard.jsx
+++ components/common/cards/site/SiteCard.jsx
@@ -1,33 +1,20 @@
 import React from "react";
-import { Text, View, Image, TouchableOpacity } from "react-native";
-import styles from "./sitecard.style";
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { stripHtmlTags } from "../../../../utils";
+import styles from "./sitecard.style";
 
-export default function SiteCard({
-  image,
-  title,
-  content,
-  google_maps_link,
-  distance,
-  handleNavigate,
-}) {
+export default function SiteCard({ image, title, content, google_maps_link, distance, handleNavigate }) {
   const plainContent = stripHtmlTags(content);
   return (
     <TouchableOpacity style={styles.container} onPress={handleNavigate}>
       <View contentContainerStyle={styles.container}>
         <Image
-          source={
-            image
-              ? { uri: image }
-              : require("../../../../assets/images/default-museum.jpg")
-          }
+          source={image ? { uri: image } : require("../../../../assets/images/default-museum.jpg")}
           style={styles.image}
         />
         <Text style={styles.siteName}>{title}</Text>
         <Text style={styles.siteText}>{plainContent}</Text>
-        {distance !== Infinity && distance != null && (
-          <Text style={styles.siteText}>Distance: {distance} km</Text>
-        )}
+        {distance !== Infinity && distance != null && <Text style={styles.siteText}>Distance: {distance} km</Text>}
       </View>
     </TouchableOpacity>
   );
