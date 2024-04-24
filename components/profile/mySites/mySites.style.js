077c82a096befff6a51838b838878cc68b34682b
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-25 02:28:05 +0300

    Added MySites for swiping action on cards

---
+++ components/profile/mySites/mySites.style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../../../constants";
+
+const styles = StyleSheet.create({
+  postContainer: {
+    backgroundColor: COLORS.lightWhite,
+    borderRadius: SIZES.small,
+    margin: SIZES.large,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    overflow: "hidden", // You might not need overflow hidden here
+    padding: 50,
+  },
+  image: {
+    width: "100%",
+    height: 200, // Fixed height for images
+    resizeMode: "cover",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 10,
+  },
+});
