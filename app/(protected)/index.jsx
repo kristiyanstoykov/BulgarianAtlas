79cdb2a83d9d00798fc52da2df30ff6cced1c9fe
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-04-24 15:22:29 +0300

    Updated fetching posts in the home screen

--- app/(protected)/index.jsx
+++ app/(protected)/index.jsx
@@ -1,15 +1,45 @@
-import React from "react";
-import { View, SafeAreaView, Text, ScrollView, StyleSheet } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  Text,
+  ScrollView,
+  StyleSheet,
+  RefreshControl,
+} from "react-native";
 import { Sites } from "../../components";
 import { SIZES, FONT, COLORS, icons, images } from "../../constants";
+import { fetchSites } from "../../hooks/fetchSites";
 
 export default function HomeScreen() {
+  const [betterData, setBetterData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    fetchSites(setBetterData, setIsLoading, setError);
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchSites(setBetterData, setIsLoading, setError).finally(() =>
+      setRefreshing(false)
+    );
+  }, []);
+
   return (
     <SafeAreaView>
-      <ScrollView showsHorizontalScrollIndicator={true}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        showsHorizontalScrollIndicator={false}
+      >
         <View style={styles.container}>
           <Text style={styles.header}>Bulgarian culture</Text>
-          <Sites />
+          <Sites data={betterData} isLoading={isLoading} error={error} />
         </View>
       </ScrollView>
     </SafeAreaView>
