e3e27e186930b72fbf304a79559f0e36fc2db40d
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-04-25 04:42:49 +0300

    Updated import

--- constants/icons.js
+++ constants/icons.js
@@ -8,7 +8,7 @@ import share from "../assets/icons/share.png";
 import location from "../assets/icons/location.png";
 import chevronLeft from "../assets/icons/chevron-left.png";
 import chevronRight from "../assets/icons/chevron-right.png";
-import bulgarianAtlas from "../assets/icons/favicon.png";
+import bulgarianAtlas from "../assets/favicon.png";
 
 export default {
   heart,
